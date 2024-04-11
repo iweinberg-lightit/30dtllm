d371734c9005529d18cb487b180a4cb033deddb2
Iván Ariel Weinberg <161361800+iweinberg-lightit@users.noreply.github.com>
2024-04-11 10:24:28 -0300

    Add pivot table in database/migrations/2024_04_11_123802_create_tags_table.php file.

--- database/migrations/2024_04_11_123802_create_tags_table.php
+++ database/migrations/2024_04_11_123802_create_tags_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Job;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,17 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
+
+        Schema::create('job_tag ', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Job::class);
+            $table->foreignIdFor(\App\Models\Tag ::class);
+            $table->timestamps();
+        });
+
     }
 
     /**
